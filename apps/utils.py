84191ebd67eea96a5a4fafbcaacf268cedae6c3d
MavlonovAbduraxmon <mavlonovabduraxmon544@gmail.com>
2025-10-09 09:48:30 +0500

    add models

--- apps/utils.py
+++ apps/utils.py
@@ -13,7 +13,6 @@ def _get_login_key(phone):
 
 def send_sms_code(phone: str, code: int, expire_time=60):
     print(f"[TEST] Phone: {phone} == Sms code: {code}")
-    # TODO send sms
     _key = _get_login_key(phone)
     cache.set(_key, code, expire_time)
 
