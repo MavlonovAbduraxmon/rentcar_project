5c6f7ec800b3d06c0c02b73f3af804ae0075bcd2
MavlonovAbduraxmon <mavlonovabduraxmon544@gmail.com>
2025-11-06 11:54:17 +0500

    Merge remote-tracking branch 'origin/master'

--- apps/urls.py
+++ apps/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
-
 from apps.views import (BrandListCreateAPIView, CarListCreateAPIView,
                         CategoryListCreateAPIView, LoginAPIView,
-                        NewsListCreateAPIView, SendCodeAPIView, CarRetrieveUpdateDestroyAPIView, VerifyCodeAPIView)
+                        NewsListCreateAPIView, SendCodeAPIView, CarRetrieveUpdateDestroyAPIView, VerifyCodeAPIView,
+                        LongTermRentalHistoryListAPIView, UserProfileCreateAPIView)
 
 urlpatterns = [
-    path('auth/send-code', SendCodeAPIView.as_view(), name='token_obtain_pair'),
-    path('auth/verify-code', VerifyCodeAPIView.as_view(), name='token_obtain_pair'),
-    path('auth/login', LoginAPIView.as_view(), name='register'),
+    path('auth/send-code', SendCodeAPIView.as_view(), name='send_code'),
+    path('auth/verify-code', VerifyCodeAPIView.as_view(), name='verify_code'),
+    path('auth/login', LoginAPIView.as_view(), name='login'),
+    # path('user/register', UserProfileCreateAPIView.as_view(), name='user_profile'),
     path('news', NewsListCreateAPIView.as_view(), name='new-list'),
     path('categories', CategoryListCreateAPIView.as_view(), name='category-list'),
     path('cars', CarListCreateAPIView.as_view(), name='car_model'),
     path('cars/<uuid:uuid>', CarRetrieveUpdateDestroyAPIView.as_view(), name='car_detail'),
+    path('rentals/history', LongTermRentalHistoryListAPIView.as_view(), name='rents_history'),
     path('brands', BrandListCreateAPIView.as_view(), name='brand-list'),
 ]
